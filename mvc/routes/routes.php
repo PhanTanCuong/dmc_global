d5cb4cffb915fa4104fc7adb4e791950ed3f62db
PhanTanCuong <phantancuong2002@gmail.com>
2024-08-05 13:27:24 +0700

    Signout
    logOut route

--- mvc/routes/routes.php
+++ mvc/routes/routes.php
@@ -2,6 +2,7 @@
 use Core\Route;
 Route::add('/','Home@display');
 Route::add('Signin','Signin@display');
+Route::add('Signout','Signin@logout');
 Route::add('Register','Register@display');
 Route::add('Register/signup','Register@signup');
 Route::add('Signin/login','Signin@login');
