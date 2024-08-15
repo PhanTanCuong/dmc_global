25d957e92652e1228fcd79b21e9cf71b08b9f4d7
PhanTanCuong <phantancuong2002@gmail.com>
2024-08-15 10:04:32 +0700

    move index to public folder

--- mvc/Bridge.php
+++ mvc/Bridge.php
@@ -1,10 +1,10 @@
 <?php
-require_once "./mvc/core/App.php";
-require_once "./mvc/core/Controller.php";
-require_once "./mvc/core/DB.php";
-require_once "./mvc/core/Route.php";
-require_once "./mvc/core/Middleware.php";
-require_once "./mvc/routes/routes.php";
+require_once "../mvc/core/App.php";
+require_once "../mvc/core/Controller.php";
+require_once "../mvc/core/DB.php";
+require_once "../mvc/core/Route.php";
+require_once "../mvc/core/Middleware.php";
+require_once "../mvc/routes/routes.php";
 
 // autoload.php @generated  by Composer
 require __DIR__ . '/../vendor/autoload.php';
--- mvc/core/App.php
+++ mvc/core/App.php
@@ -10,29 +10,6 @@ class App
 
     function __construct()
     {
-        //    //Xu Ly Controller
-        //    //ucfirst($arr[0]):'Make a string's first character uppercase
-        //    if(file_exists("./mvc/controllers/". $arr[0].".php")){
-        //         $this->controller=$arr[0]; //end user nhập đầu vao Controller gì thì vẫn qua trang chủ user
-        //         unset($arr[0]);
-        //     }
-
-        //     require_once "./mvc/controllers/". $this->controller.".php";
-        //     $this->controller= new $this->controller; //khởi tạo đối tượng controller
-        //     //Xử lý Action
-        //     if(isset($arr[1])){   
-        //         if( method_exists($this->controller,$arr[1]) ){
-        //             $this->action=$arr[1];    
-        //         }
-        //         unset($arr[1]);
-
-        //     }
-        //     //Xử lý Params
-        //     $this->params=$arr?array_values($arr) : [];
-        //     //Hàm tạo ra biến có kiểu lớp là controller(...)
-        //     call_user_func_array(  [$this->controller,$this->action], $this->params);
-
-
         $arr = $this->UrlProcess();
 
         // Nếu URL là rỗng , gán URL là '/'
