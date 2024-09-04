9b7072eefbff18897bf5025b20a864a3ddaa59cc
PhanTanCuong <phantancuong2002@gmail.com>
2024-09-04 16:59:19 +0700

    fetch by category level by parent item

--- mvc/models/CategoryModel.php
+++ mvc/models/CategoryModel.php
@@ -8,14 +8,22 @@ class CategoryModel extends DB
     public function getInforCategory()
     {
         try {
-            $query = "SELECT * FROM product_category";
+            $query = "SELECT *
+                        FROM product_category
+                        ORDER BY 
+                            CASE 
+                                WHEN parent_id = 0 THEN id
+                                ELSE parent_id
+                            END,
+                            parent_id, 
+                            id";
             return mysqli_query($this->connection, $query);
         } catch (mysqli_sql_exception $e) {
             echo $e->getMessage();
         }
     }
 
-    
+
     public function getSlugParent()
     {
         try {
@@ -26,12 +34,12 @@ class CategoryModel extends DB
         }
     }
 
-    public function addCategoryInfor($name,$slug,$parent_id)
+    public function addCategoryInfor($name, $slug, $parent_id)
     {
         try {
             $query = "INSERT INTO product_category (type,slug,parent_id) VALUES (?,?,?)";
             $stmt = $this->connection->prepare($query);
-            $stmt->bind_param("ssi", $name,$slug,$parent_id);
+            $stmt->bind_param("ssi", $name, $slug, $parent_id);
             $stmt->execute();
             return $stmt;
         } catch (mysqli_sql_exception $e) {
@@ -48,15 +56,16 @@ class CategoryModel extends DB
             echo $e->getMessage();
         }
     }
-    public function customizeInforCategory($id, $name,$slug,$parent_id)
+    public function customizeInforCategory($id, $name, $slug, $parent_id)
     {
         try {
             $query = "UPDATE product_category SET type=?,slug=?,parent_id=? WHERE id = ?";
             $stmt = $this->connection->prepare($query);
-            $stmt->bind_param("ssii", $name,$slug,$parent_id,$id);
-            if(!$stmt->execute()){
+            $stmt->bind_param("ssii", $name, $slug, $parent_id, $id);
+            if (!$stmt->execute()) {
                 return false;
-            };
+            }
+            ;
 
             return true;
         } catch (mysqli_sql_exception $e) {
@@ -75,19 +84,20 @@ class CategoryModel extends DB
         }
     }
 
-    public function traceParent(int $category_id,int $level = 0) {
+    public function traceParent(int $category_id, int $level = 0)
+    {
         //Find a atribute with id=parent_id
         $query = "SELECT * FROM product_category WHERE id = $category_id LIMIT 1";
         $result = mysqli_query($this->connection, $query);
         $category = mysqli_fetch_assoc($result);
-    
+
         //category not found
         if (!$category) {
             return false;
         }
         // parent_id=0==>reached the root==>return the current level
         if ($category['parent_id'] == 0) {
-        return $level+1;
+            return $level + 1;
         } else {
             // parent_id <> 0, continue trace the parent category
             return $this->traceParent($category['parent_id'], $level + 1);
