dcafe95241403171a7704c2a70b42fd932d58b69
PhanTanCuong <phantancuong2002@gmail.com>
2024-09-05 13:48:42 +0700

    Fixxing
    Rename product_category table  into category table

--- mvc/models/CategoryModel.php
+++ mvc/models/CategoryModel.php
@@ -9,7 +9,7 @@ class CategoryModel extends DB
     {
         try {
             $query = "SELECT *
-                        FROM product_category
+                        FROM category
                         ORDER BY 
                             CASE 
                                 WHEN parent_id = 0 THEN id
@@ -26,7 +26,7 @@ class CategoryModel extends DB
     public function getInforProductCategory()
     {
         try {
-            $query = "SELECT * FROM product_category WHERE level=1 AND parent_id=23";
+            $query = "SELECT * FROM category WHERE level=1 AND parent_id=23";
             return mysqli_query($this->connection, $query);
         } catch (mysqli_sql_exception $e) {
             echo "Error: " . $e->getMessage();
@@ -36,7 +36,7 @@ class CategoryModel extends DB
     public function addCategoryInfor($name, $slug, $parent_id, $level)
     {
         try {
-            $query = "INSERT INTO product_category (type,slug,parent_id,level) VALUES (?,?,?,?)";
+            $query = "INSERT INTO category (type,slug,parent_id,level) VALUES (?,?,?,?)";
             $stmt = $this->connection->prepare($query);
             $stmt->bind_param("ssii", $name, $slug, $parent_id, $level);
             if ($stmt->execute()) {
@@ -51,7 +51,7 @@ class CategoryModel extends DB
     public function getCategoryById($id)
     {
         try {
-            $query = "SELECT * FROM product_category WHERE id='$id'";
+            $query = "SELECT * FROM category WHERE id='$id'";
             return mysqli_query($this->connection, $query);
         } catch (mysqli_sql_exception $e) {
             echo $e->getMessage();
@@ -60,7 +60,7 @@ class CategoryModel extends DB
     public function customizeInforCategory($id, $name, $slug, $parent_id, $level)
     {
         try {
-            $query = "UPDATE product_category SET type=?,slug=?,parent_id=?,level=? WHERE id = ?";
+            $query = "UPDATE category SET type=?,slug=?,parent_id=?,level=? WHERE id = ?";
             $stmt = $this->connection->prepare($query);
             $stmt->bind_param("ssiii", $name, $slug, $parent_id, $level, $id);
             if ($stmt->execute()) {
@@ -76,7 +76,7 @@ class CategoryModel extends DB
     public function deleteCategory($id)
     {
         try {
-            $query = "DELETE FROM product_category WHERE id='$id'";
+            $query = "DELETE FROM category WHERE id='$id'";
             return mysqli_query($this->connection, $query);
         } catch (mysqli_sql_exception $e) {
             echo $e->getMessage();
@@ -86,7 +86,7 @@ class CategoryModel extends DB
     public function traceParent(int $category_id, int $level = 0)
     {
         //Find a atribute with id=parent_id
-        $query = "SELECT * FROM product_category WHERE id = $category_id LIMIT 1";
+        $query = "SELECT * FROM category WHERE id = $category_id LIMIT 1";
         $result = mysqli_query($this->connection, $query);
         $category = mysqli_fetch_assoc($result);
 
@@ -106,7 +106,7 @@ class CategoryModel extends DB
     public function hasChildren($category_id)
     {
         try {
-            $query = "SELECT COUNT(*) AS child_count FROM product_category WHERE parent_id = ?";
+            $query = "SELECT COUNT(*) AS child_count FROM category WHERE parent_id = ?";
             $stmt = $this->connection->prepare($query);
             $stmt->bind_param("i", $category_id);
             $stmt->execute();
