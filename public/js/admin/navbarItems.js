9f0138bf5fc15b968f3eb58760891e1e2e1a04a4
PhanTanCuong <phantancuong2002@gmail.com>
2024-09-06 11:35:45 +0700

    dragNdrop.js

--- public/js/admin/navbarItems.js
+++ public/js/admin/navbarItems.js
@@ -92,18 +92,3 @@ $('#childItemCheckbox').change(function() {
   }
 });
 
-// drag and drop functionality (optional to extend)
-$('.draggable-item').on('dragstart', function(event) {
-  event.originalEvent.dataTransfer.setData('text', event.target.dataset.id);
-});
-
-$('#selectedItems').on('dragover', function(event) {
-  event.preventDefault();
-});
-
-$('#selectedItems').on('drop', function(event) {
-  event.preventDefault();
-  var data = event.originalEvent.dataTransfer.getData('text');
-  var draggedItem = $('[data-id="' + data + '"]');
-  $('#selectedItems').append(draggedItem);
-});
