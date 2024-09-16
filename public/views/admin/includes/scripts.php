e3fe518e7a2dbdbcc959ad70cb0a3038e50a2848
PhanTanCuong <phantancuong2002@gmail.com>
2024-09-16 13:58:39 +0700

    Make a notification toasts

--- public/views/admin/includes/scripts.php
+++ public/views/admin/includes/scripts.php
@@ -22,6 +22,8 @@
 
 <!-- Summernote Editor -->
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+<!-- Toast -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <script>
   function redirectTo(path) {
     // <!-- The link that will trigger the JavaScript function to redirect -->
